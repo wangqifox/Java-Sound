d6e22dbaa6a7c71287c0fdf8100396d653fda2c4
wangqi <wangq2880@sina.com>
2017-02-09 23:30:42 +0800

    fix

--- src/main/java/love/wangqi/CapturePlayback.java
+++ src/main/java/love/wangqi/CapturePlayback.java
@@ -4,6 +4,7 @@ import javax.sound.sampled.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by wangqi on 2017/2/9.
@@ -16,79 +17,129 @@ public class CapturePlayback {
     float frameRate = sampleRate;
     boolean bigEndian = false;
 
-    public void run() {
-        TargetDataLine line = null;
+    AudioFormat format = new AudioFormat(
+            AudioFormat.Encoding.PCM_SIGNED,
+            sampleRate,
+            sampleSizeInBits,
+            channels,
+            frameSize,
+            frameRate,
+            bigEndian
+    );
 
-        AudioFormat format = new AudioFormat(
-                AudioFormat.Encoding.PCM_SIGNED,
-                sampleRate,
-                sampleSizeInBits,
-                channels,
-                frameSize,
-                frameRate,
-                bigEndian
-        );
-
-        System.out.println(format);
-
-        DataLine.Info info = new DataLine.Info(
-                TargetDataLine.class,
-                format
-        );
-
-        System.out.println(AudioSystem.isLineSupported(info));
-
-        // get and open the target data line for capture.
-        try {
-            line = (TargetDataLine) AudioSystem.getLine(info);
-            line.open(format, line.getBufferSize());
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        }
-
-        // play back the captured audio data
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        int frameSizeInBytes = format.getFrameSize();
-        int bufferLengthInFrames = line.getBufferSize() / 8;
-        int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
-        byte[] data = new byte[bufferLengthInBytes];
+    class Data {
+        byte[] data;
         int numBytesRead;
 
-        line.start();
-        int i = 0;
-        while (i < 20) {
-            if((numBytesRead = line.read(data, 0, bufferLengthInBytes)) == -1) {
-                break;
+        public Data(byte[] data, int numBytesRead) {
+            this.data = data;
+            this.numBytesRead = numBytesRead;
+        }
+    }
+    LinkedBlockingQueue<Data> queue = new LinkedBlockingQueue<>();
+
+
+    class Capture implements Runnable {
+
+        @Override
+        public void run() {
+            TargetDataLine targetDateLine = null;
+            DataLine.Info targetInfo = new DataLine.Info(
+                    TargetDataLine.class,
+                    format
+            );
+
+            // get and open the target data line for capture.
+            try {
+                targetDateLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+                targetDateLine.open(format, targetDateLine.getBufferSize());
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
             }
-            out.write(data, 0, numBytesRead);
-            i++;
-            System.out.println(i);
+
+            // play back the captured audio data
+            int frameSizeInBytes = format.getFrameSize();
+            int bufferLengthInFrames = targetDateLine.getBufferSize() / 8;
+            int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
+            byte[] data = new byte[bufferLengthInBytes];
+            int numBytesRead;
+
+            targetDateLine.start();
+
+            int i = 0;
+            while (true) {
+                if((numBytesRead = targetDateLine.read(data, 0, bufferLengthInBytes)) == -1) {
+                    break;
+                }
+
+                try {
+                    queue.put(new Data(data, numBytesRead));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(i++);
+            }
+
+            targetDateLine.stop();
+            targetDateLine.close();
+
         }
 
-        line.stop();
-        line.close();
+        public void start() {
+            Thread thread = new Thread(this);
+            thread.setName("Capture");
+            thread.start();
+        }
+    }
 
-        // stop and close the output stream
-        try {
-            out.flush();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    class Playback implements Runnable {
+
+        @Override
+        public void run() {
+            SourceDataLine sourceDataLine = null;
+            DataLine.Info sourceInfo = new DataLine.Info(
+                    SourceDataLine.class,
+                    format
+            );
+
+            try {
+                sourceDataLine = (SourceDataLine) AudioSystem.getLine(sourceInfo);
+                sourceDataLine.open(format);
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
+
+            sourceDataLine.start();
+
+            while (true) {
+                Data data = null;
+                try {
+                    data = queue.take();
+                    sourceDataLine.write(data.data, 0, data.numBytesRead);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+//            sourceDataLine.drain();
+//            sourceDataLine.close();
+
         }
 
-        // load bytes into the audio input stream for playback
-        byte audioBytes[] = out.toByteArray();
-        ByteArrayInputStream bais = new ByteArrayInputStream(audioBytes);
-
-        AudioInputStream audioInputStream = new AudioInputStream(bais, format, audioBytes.length / frameSizeInBytes);
-
-        try {
-            audioInputStream.reset();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public void start() {
+            Thread thread = new Thread(this);
+            thread.setName("Playback");
+            thread.start();
         }
+    }
 
-        new PlaySounds().play(audioInputStream);
+    public void run() {
+        System.out.println(format);
+        new Capture().start();
+        new Playback().start();
     }
 
     public static void main(String[] args) {
